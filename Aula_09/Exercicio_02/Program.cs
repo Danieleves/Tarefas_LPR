fe0db501374d00ae34c81c127d256519be812b8f
Danieleves <gabrielvasconcelos073965@gmail.com>
2025-07-11 09:33:52 -0300

    main

---
+++ Aula_09/Exercicio_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        List<string> nommes = new List<int> {};
+    }
+}
+
+/*Construa uma lista de X nomes aleatórios
+A saída deve ser mostrada em uma ou
+mais linhas. Cada linha tem uma lista de
+nomes ordenados por tamanho,
+começando com o menor. Em cada linha,
+só pode ser mostrado um nome de
+determinado tamanho, e os demais
+nomes com o mesmo tamanho devem ser
+apresentados nas linhas seguintes. Você
+deve seguir a ordem de digitação.*/
---
+++ Aula_09/Exercicio_03/Program.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main()
+{
+  map<int, int> numAleatorios;
+
+
+}
+
+
+
+/*Construa uma lista de 100 números
+aleatórios
+Crie um algoritmo que coloque-os em
+ordem crescente e imprima-os
+A partir dessa lista ordenada, remova
+todos os números pares e imprima a lista
+novamente.
+Por fim imprima quais números se
+repetem, se existe algum número
+repetido.*/
+
